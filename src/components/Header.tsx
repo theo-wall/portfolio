06321d3d9e6dd4345d3396948b7487e49ada4db8
theo-wall <theo.wall@propra.ca>
2022-07-12 11:41:29 -0600

    added redirects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,63 +1,26 @@
 import Typed from "typed.js";
 import { useEffect } from "react";
+import "../constants";
+import { Redirect, gitHub, linkedIn, mail } from "../constants";
 
 type HeaderProps = {
   welcomeMessage: string;
   subText: string;
 };
 
-const gitHub = `
-                :~7JY5PPPP5YJ7~:                 
-             .~JG#&&&&&&&&&&&&&&#GJ~.             
-           :JB&@&&&&&&&&&&&&&&&&&&@&B?:           
-         .J#&&&BB#&&&&&&&&&&&&&&#BB&&&#?.         
-        ^B&&&&P .:!YP5YJJJJY5PY!:..G&&&&G^        
-       ~#&&&&&Y                    5&&&&&B^       
-      .B&&&&&&?                    J&&&&&&G.      
-      ?&&&&&&7                      ?&&&&&&7      
-      P&&&&&B.                      :#&&&&&5      
-      G&&&&&#:                      ^&&&&&&P      
-      Y&&&&&&J                      Y&&&&&&J      
-      ^&&&&&&&J.                  .Y&&&&&&#:      
-       ?&&&P5B&#Y!^:.       ..:^75#&&&&&&&7       
-        ?&&BJ^?#@@&&5.      .P#&&&&&&&&&#7        
-         ~G&@G^^7JYJ^        7&&&&&&&&&G^         
-           7G&#5?!!7:        !&&&&&&&G!           
-             ~YB&@@@!        7@&&#GJ^             
-                ^!JY:        ^5J!:
-`;
-
-const linkedIn = `
-:JYYYYYYYYYYYYYYYYYYYYYYJ:    
-?P55?!!?5555555555555555P?    
-?PP!    ~555555555555555P?    
-?P5J~::^J5PPPP5PPPPPP555P?    
-?P557~~!557!!!J?!~~~7Y55P?    
-?P5Y    Y5.           ?5P?    
-?P5Y.   Y5.   ^YYY~   .5P?    
-?P5Y.   Y5.   JP5PJ   .YP?    
-?P5Y.   Y5.   J555J   .YP?    
-?P5Y    Y5.   J555J    YP?    
-?P557!!!557!!!Y555Y!!!75P?    
-:JYYYYYYYYYYYYYYYYYYYYYYJ:    
-`;
-
-const mail = `
-.:^^:              :7??~             
-.!!!!!!~.         ^?GGGGG?            
-:7!!!!7??!^    .~7?YGGGGPJ            
-:J?7!!!????7~^!????YGGPYJ7            
-:JJJJ?7????????????J5JJJJ7            
-:JJJJJ7.~7???????7:^JJJJJ7            
-:JJJJJ7   ^7???~:  :JJJJJ7            
-:JJJJJ7     :^.    :JJJJJ7            
-:JJJJJ7            ^JJJJJ7            
-.?JJJJ!            .?JJJJ~            
- .^~~:              .^~~:             
-
-`;
-
 const Header = (props: HeaderProps) => {
+  const redirectHandler = (redirectTo: string): void => {
+    if (redirectTo === Redirect.GITHUB) {
+      window.open("https://github.com/Theo-Wall");
+    }
+    if (redirectTo === Redirect.LINKEDIN) {
+      window.open("https://www.linkedin.com/in/theo-wall-398452211/");
+    }
+    if (redirectTo === Redirect.MAIL) {
+      window.open("mailto:theo.wall11@gmail.com");
+    }
+  };
+
   const options1 = {
     strings: [
       "Welcome to my site blah blah blah I'm still working on it but I want a long string here so it looks cool",
@@ -90,14 +53,35 @@ const Header = (props: HeaderProps) => {
     <>
       <div className="terminal-header">
         <div className="logo-container">
-          <div className="logos">
-            <pre className="gitHub-logo">{gitHub}</pre>
+          <div>
+            <pre
+              className="gitHub-logo"
+              onClick={() => {
+                redirectHandler("github");
+              }}
+            >
+              {gitHub}
+            </pre>
           </div>
-          <div className="logos">
-            <pre className="linkedIn-logo">{linkedIn}</pre>
+          <div>
+            <pre
+              className="linkedIn-logo"
+              onClick={() => {
+                redirectHandler("linkedIn");
+              }}
+            >
+              {linkedIn}
+            </pre>
           </div>
-          <div className="logos">
-            <pre className="mail-logo">{mail}</pre>
+          <div>
+            <pre
+              className="mail-logo"
+              onClick={() => {
+                redirectHandler("mail");
+              }}
+            >
+              {mail}
+            </pre>
           </div>
         </div>
         <pre className="terminal-banner">{props.welcomeMessage}</pre>
---
+++ src/constants.tsx
@@ -0,0 +1,55 @@
+export enum Redirect {
+  GITHUB = "github",
+  LINKEDIN = "linkedIn",
+  MAIL = "mail",
+}
+
+export const gitHub = `
+                :~7JY5PPPP5YJ7~:                 
+             .~JG#&&&&&&&&&&&&&&#GJ~.             
+           :JB&@&&&&&&&&&&&&&&&&&&@&B?:           
+         .J#&&&BB#&&&&&&&&&&&&&&#BB&&&#?.         
+        ^B&&&&P .:!YP5YJJJJY5PY!:..G&&&&G^        
+       ~#&&&&&Y                    5&&&&&B^       
+      .B&&&&&&?                    J&&&&&&G.      
+      ?&&&&&&7                      ?&&&&&&7      
+      P&&&&&B.                      :#&&&&&5      
+      G&&&&&#:                      ^&&&&&&P      
+      Y&&&&&&J                      Y&&&&&&J      
+      ^&&&&&&&J.                  .Y&&&&&&#:      
+       ?&&&P5B&#Y!^:.       ..:^75#&&&&&&&7       
+        ?&&BJ^?#@@&&5.      .P#&&&&&&&&&#7        
+         ~G&@G^^7JYJ^        7&&&&&&&&&G^         
+           7G&#5?!!7:        !&&&&&&&G!           
+             ~YB&@@@!        7@&&#GJ^             
+                ^!JY:        ^5J!:
+`;
+
+export const linkedIn = `
+:JYYYYYYYYYYYYYYYYYYYYYYJ:    
+?P55?!!?5555555555555555P?    
+?PP!    ~555555555555555P?    
+?P5J~::^J5PPPP5PPPPPP555P?    
+?P557~~!557!!!J?!~~~7Y55P?    
+?P5Y    Y5.           ?5P?    
+?P5Y.   Y5.   ^YYY~   .5P?    
+?P5Y.   Y5.   JP5PJ   .YP?    
+?P5Y.   Y5.   J555J   .YP?    
+?P5Y    Y5.   J555J    YP?    
+?P557!!!557!!!Y555Y!!!75P?    
+:JYYYYYYYYYYYYYYYYYYYYYYJ:    
+`;
+
+export const mail = `
+.:^^:              :7??~             
+.!!!!!!~.         ^?GGGGG?            
+:7!!!!7??!^    .~7?YGGGGPJ            
+:J?7!!!????7~^!????YGGPYJ7            
+:JJJJ?7????????????J5JJJJ7            
+:JJJJJ7.~7???????7:^JJJJJ7            
+:JJJJJ7   ^7???~:  :JJJJJ7            
+:JJJJJ7     :^.    :JJJJJ7            
+:JJJJJ7            ^JJJJJ7            
+.?JJJJ!            .?JJJJ~            
+ .^__:              .^__:             
+`;
